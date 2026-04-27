dd43030fa4119bee2c6930cac72d68f764b60176
John <nikitin.e.d@gmail.com>
2026-04-27 14:44:14 +0400

    Добавлены FoodEntity.kt, FoodDiaryEntity.kt, FoodDao.kt

---
+++ app/src/main/java/com/niked/fatless/data/local/entities/FoodEntity.kt
@@ -0,0 +1,17 @@
+package com.niked.fatless.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "food_items")
+data class FoodEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val proteins: Float, // на 100г
+    val fats: Float,     // на 100г
+    val carbs: Float,    // на 100г
+    val calories: Int,   // на 100г
+    val category: String = "Общее",
+    val isCustom: Boolean = false // Создано юзером или системное
+)
